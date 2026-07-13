b8b07d878d20590790ee202c8ba062a0fd226cb7
Michae12 <velichkomisha5@gmail.com>
2026-07-14 02:00:08 +0300

    Update constants.js

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,1 +1,7 @@
+import * as FileSystem from 'expo-file-system';
 
+export const SECURE_KEY_ALIAS = "AQ.Ab8RN6JjHcuOOg-U0O5CQx9KD7FVjjKUVT70-HoVTK2sy5cgVg";
+export const FIXED_CATEGORIES = ["Входящие", "Учеба", "Работа", "Личное", "Здоровье", "Финансы"];
+export const RESERVED_NAMES = ["attachments", ".trp_system"];
+export const TMP_DIR = `${FileSystem.documentDirectory}.trp_system/`;
+export const FINANCE_JSON_PATH = `${FileSystem.documentDirectory}Финансы/data.json`;
