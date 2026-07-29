7726496dc5c2328119771ddfb09021aeda4bfbd9
Michae12 <velichkomisha5@gmail.com>
2026-07-30 02:52:52 +0300

    Update constants.js

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,7 +1,26 @@
-import * as FileSystem from 'expo-file-system';
+export const FIXED_CATEGORIES = [
+  'Входящие',
+  'Учеба',
+  'Работа',
+  'Личное',
+  'Здоровье',
+  'Финансы'
+];
 
-export const SECURE_KEY_ALIAS = "AQ.Ab8RN6JjHcuOOg-U0O5CQx9KD7FVjjKUVT70-HoVTK2sy5cgVg";
-export const FIXED_CATEGORIES = ["Входящие", "Учеба", "Работа", "Личное", "Здоровье", "Финансы"];
-export const RESERVED_NAMES = ["attachments", ".trp_system"];
-export const TMP_DIR = `${FileSystem.documentDirectory}.trp_system/`;
-export const FINANCE_JSON_PATH = `${FileSystem.documentDirectory}Финансы/data.json`;
+export const SECURE_KEY_ALIAS = 'gemini_api_key';
+export const GOOGLE_TOKEN_ALIAS = 'google_access_token';
+export const WALLET_STORAGE_KEY = 'wallet_transactions_data';
+export const SUBSCRIPTIONS_STORAGE_KEY = 'wallet_subscriptions_data';
+
+export const COLORS = {
+  bgPrimary: '#0F071E',
+  surface: '#1E1135',
+  border: '#4C1D95',
+  accentPrimary: '#7C3AED',
+  accentSecondary: '#8B5CF6',
+  textPrimary: '#DDD6FE',
+  textMuted: '#8B7BA8',
+  success: '#34D399',
+  danger: '#BE185D',
+  cardBg: '#180B2B'
+};
